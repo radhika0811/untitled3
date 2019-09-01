7a51af1ac0f6e93ec67e59f605a52c8cbf4599d0
radhika0811 <radhika.agrawal_cs18@gla.ac.in>
2019-09-01 14:28:28 -0700

    Initial commit

---
+++ src/Abc.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Abc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        System.out.print( (a-32)/(1.8));
+    }
+}
